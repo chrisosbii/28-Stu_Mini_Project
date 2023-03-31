eba5800582a00882d43671563410d62b8c018f04
chrisosbii <chrisosbii@gmail.com>
2023-03-30 21:01:49 -0500

    added form object from 24

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
-//time functions here
+/* Global Variable */
 var today = dayjs();
+var form = $('#form');
+var nameInputEl = $('#skill-name');
+var dateInputEl = $('#datepicker');
+var skillsListEl = $('#skills-list');
+
+/* Functions */
+
+/**
+ * Current Time updates the current time object with the latest time
+ */
+function currentTime(){
+
+}
